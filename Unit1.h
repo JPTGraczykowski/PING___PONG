5d55ddfac47584791edfceeae0f3dc29eeb63afa
JPTGraczykowski <jpt.graczykowski@gmail.com>
2020-05-19 22:49:55 +0200

    New game and next round after fail

--- Unit1.h
+++ Unit1.h
@@ -16,25 +16,31 @@ __published:	// IDE-managed Components
         TShape *Background;
         TImage *PaddleRight;
         TImage *PaddleLeft;
-        TTimer *PaddleRightTimerUp;
+        TImage *Ball;
         TTimer *PaddleLeftTimerUp;
+        TTimer *PaddleRightTimerUp;
         TTimer *PaddleLeftTimerDown;
         TTimer *PaddleRightTimerDown;
-        TImage *Ball;
         TTimer *BallTimer;
         TLabel *LabelStart;
         TButton *ButtonStart;
+        TButton *ButtonNewGame;
+        TButton *ButtonNextRound;
+        TLabel *LabelWhoWins;
+        TLabel *LabelScore;
+        TLabel *LabelBounces;
         void __fastcall FormCreate(TObject *Sender);
-        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
-          TShiftState Shift);
         void __fastcall PaddleLeftTimerUpTimer(TObject *Sender);
         void __fastcall PaddleLeftTimerDownTimer(TObject *Sender);
         void __fastcall PaddleRightTimerUpTimer(TObject *Sender);
         void __fastcall PaddleRightTimerDownTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
         void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
           TShiftState Shift);
         void __fastcall BallTimerTimer(TObject *Sender);
         void __fastcall ButtonStartClick(TObject *Sender);
+        void __fastcall ButtonNextRoundClick(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
         __fastcall TForm1(TComponent* Owner);
